cc7190d527799eb775173a355523bd279846d211
Raees <mraiskundi@gmail.com>
2024-03-31 22:32:01 +0500

    Card Generator using DOM and Append method

---
+++ CardGenerator/main.js
@@ -0,0 +1,25 @@
+const card = document.querySelector(".card");
+const container = document.querySelector(".container");
+let count = 1;
+card.addEventListener("click", () => {
+  const newcard = document.createElement("div");
+  newcard.classList.add("heading");
+  newcard.innerText = count++;
+  container.append(newcard);
+});
+
+// for (let i = 1; i <= 100; i++) {
+//   const newimage = firstimage.cloneNode();
+//   newimage.src = ``;
+//   container.append(newimage);
+// }
+
+// for (let i = 2; i <= 100; i++) {
+//   const newimage = document.createElement("img");
+//   const paragraph = document.createElement("span");
+
+//   newimage.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
+//   paragraph.innerText = i;
+//   container.append(newimage, paragraph);
+//   //   paragraph.remove();
+// }
